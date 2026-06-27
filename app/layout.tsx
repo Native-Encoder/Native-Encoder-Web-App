d5d082efc309d192d13c6c763bf2bc460040a782
Sahan Punchihewa <sahannilupul18@gmail.com>
2026-06-27 22:56:58 +0530

    initial project

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/header/index";
 import Footer from "./components/footer/index"
-import {WhatsappButton} from "./components/icons"
+import { WhatsappButton } from "./components/icons"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
